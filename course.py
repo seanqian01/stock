30618fcd7ef3b6a557f0e84983f5eb1449f26d39
seanqian01 <314792726@qq.com>
2021-02-16 16:07:54 +0800

    量化第一课，拿行情数据

--- course.py
+++ course.py
@@ -1,3 +1,7 @@
-a = int(input('a='))
-b = int(input('b='))
-print('%d + %d =%d' % (a, b, a+b))
+import ccxt
+print(ccxt.exchanges)
+bitmax_test = ccxt.binance()
+bitmax_test.fetch_ticker('BTC/USD')
+
+
+
