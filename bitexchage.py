6216893c3ddc2182d8b475399f28ca5fdb266321
314792726@qq.com <314792726@qq.com>
2021-02-16 19:38:56 +0800

    ftx官网访问最好,用它

--- bitexchage.py
+++ bitexchage.py
@@ -1,13 +1,13 @@
 import ccxt
 print(ccxt.exchanges)
-bittrex_test = ccxt.bittrex()
+ftx_test = ccxt.ftx()
 '''
 可以联通的交易所有
 bitflyer,bittrex,ftx,kraken,xena,
 '''
 Stock = "BTC/USD"
-print(bittrex_test.fetch_ticker(Stock))
-print(bittrex_test.urls)
+print(ftx_test.fetch_ticker(Stock))
+print(ftx_test.urls)
 
 
 
