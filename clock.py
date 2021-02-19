d1670bee7ad6f6bbb353eab094d5d3dfd5cd8e3b
seanqian01 <314792726@qq.com>
2021-02-19 17:29:45 +0800

    加了一个本地FMZ包

--- clock.py
+++ clock.py
@@ -1,4 +1,5 @@
 import time
+import fmz
 class AlarmClock:
     def __init__(self,triggerHour,triggerMinute):
         self.isTrigger = False
@@ -15,3 +16,27 @@ class AlarmClock:
         if day !=self.nowDay:
             self.isTrigger = False
             self.nowDay = day
+
+        if self.isTrigger == False and hour == self.TriggerHour and minute >= self.TriggerMinute:
+            self.isTrigger = True
+            return True
+
+        return False
+
+
+def main():
+    t1 = AlarmClock(14, 58)
+    t2 = AlarmClock(9, 0)
+    while True:
+        if exchange.IO("status"):
+            LogStatus(_D(), "已经连接！")
+            exchange.SetContractType("rb2105")
+            ticker = exchange.GetTicker()
+            if t1.Check():
+                Log("收盘", "#FF0000")
+
+            if t2.Check():
+                Log("开盘", "#CD32CD")
+        else:
+            LogStatus(_D(), "未连接！")
+        Sleep(500)
